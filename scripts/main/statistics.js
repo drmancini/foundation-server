439e89e7af6c884a4e0d1ff1879eceae4e560066
drmancini <michal.pobuda@me.com>
2022-04-20 23:39:15 +0200

    ads

--- scripts/main/statistics.js
+++ scripts/main/statistics.js
@@ -194,23 +194,23 @@ const PoolStatistics = function (logger, client, sequelize, poolConfig, portalCo
     }, _this.hashrateInterval * 1000);
 
     // Delete old shares cache
-    setInterval(() => {
-      sequelizeShares
-        .destroy({
-          where: {
-            share: {
-              time: {
-                [Op.lte]: (Date.now() - (_this.historicalWindow * 1000)),
-              }
-            }
-          }
-        })
-        .then(() => {
-          if (_this.poolConfig.debug) {
-            logger.debug('Statistics', _this.pool, `Finished deleting share cache for ${ blockType } configuration.`);
-          }
-        })
-    }, _this.historicalInterval * 1000);
+    // setInterval(() => {
+    //   sequelizeShares
+    //     .destroy({
+    //       where: {
+    //         share: {
+    //           time: {
+    //             [Op.lte]: (Date.now() - (_this.historicalWindow * 1000)),
+    //           }
+    //         }
+    //       }
+    //     })
+    //     .then(() => {
+    //       if (_this.poolConfig.debug) {
+    //         logger.debug('Statistics', _this.pool, `Finished deleting share cache for ${ blockType } configuration.`);
+    //       }
+    //     })
+    // }, _this.historicalInterval * 1000);
 
     // Handle Historical Data Interval
     setInterval(() => {
