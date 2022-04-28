1f7c01eccb13d3544eceb728a902cb250d16dd0e
drmancini <michal.pobuda@me.com>
2022-04-28 23:20:58 +0200

    asd

--- scripts/main/api2.js
+++ scripts/main/api2.js
@@ -987,6 +987,8 @@ const PoolApi = function (client, sequelize, poolConfigs, portalConfig) {
       workers.sort((a, b) => b.work - a.work);
       const worker = workers[0].worker;
 
+      console.log(worker);
+
       for (const [key, value] of Object.entries(results[1])) {
         if (key.split('.')[0] === address) {
           const workerData = JSON.parse(value);
