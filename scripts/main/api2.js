164ac3870af730b8eb9f2b53715ff296ac9dd39b
drmancini <michal.pobuda@me.com>
2022-05-01 17:08:36 +0200

    asd

--- scripts/main/api2.js
+++ scripts/main/api2.js
@@ -1465,7 +1465,8 @@ const PoolApi = function (client, sequelize, poolConfigs, portalConfig) {
 
   // API Endpoint for /pool/clientIP
   this.poolClientIP = function(pool, callback) {
-    const ipAddress = req.socket.remoteAddress;
+    //const ipAddress = req.socket.remoteAddress;
+    const ipAddress = 'asd';
     console.log('a: ' + ipAddress);
     callback(200, {
         result: 123,
