cb554ec67f11b9cbe78c14fa8bb373c0454c1e80
drmancini <michal.pobuda@me.com>
2022-05-28 11:08:34 +0200

    asd

--- scripts/main/statistics.js
+++ scripts/main/statistics.js
@@ -182,7 +182,7 @@ const PoolStatistics = function (logger, client, poolConfig, portalConfig) {
         logger.error('Statistics', _this.pool, `Error with statistics daemon: ${ JSON.stringify(result.error) }`);
         handler(result.error);
       } else {
-        console.log('res: ' + result.response);
+        console.log(result.response);
         const data = result.response;
         commands.push(['hset', `${ _this.pool }:statistics:${ blockType }:network`, 'difficulty', data.difficulty]);
         commands.push(['hset', `${ _this.pool }:statistics:${ blockType }:network`, 'hashrate', data.networkhashps]);
