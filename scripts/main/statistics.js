bfa7f48b4285acec19d6549453458c2ba8db77c6
drmancini <michal.pobuda@me.com>
2022-06-24 23:38:02 +0200

    asd

--- scripts/main/statistics.js
+++ scripts/main/statistics.js
@@ -6,6 +6,7 @@
 
 const https = require('https');
 const utils = require('./utils');
+const nodemailer = require("nodemailer");
 const axios = require('axios');
 const Algorithms = require('foundation-stratum').algorithms;
 
@@ -201,6 +202,39 @@ const PoolStatistics = function (logger, client, poolConfig, portalConfig) {
     });
   };
 
+  this.mailer = async function () {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount();
+  
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "localhost",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      // auth: {
+      //   user: testAccount.user, // generated ethereal user
+      //   pass: testAccount.pass, // generated ethereal password
+      // },
+    });
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Raptoreum zone" <info@raptoreum.zone>', // sender address
+      to: "michal.pobuda@me.com", // list of receivers
+      subject: "Hello", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  };
+
   // Handle Offline Workers in Redis
   this.handleOfflineWorkers = function (blockType, callback, handler) {
     const workerLookups = [
@@ -255,6 +289,7 @@ const PoolStatistics = function (logger, client, poolConfig, portalConfig) {
             };            
             commands.push(['hset', `${ _this.pool }:workers:${ blockType }:shared`, worker.worker, JSON.stringify(workerObject)]);
             console.log('Worker ' + worker.worker + ' is offline ... sending an email alert');
+            _this.mailer().catch(console.error);
           }
         });
       }); 
