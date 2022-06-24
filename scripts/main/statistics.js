564cc0c46f8afa74d54c26c409d6dcf1e70b1b14
drmancini <michal.pobuda@me.com>
2022-06-24 15:12:11 +0200

    ads

--- scripts/main/statistics.js
+++ scripts/main/statistics.js
@@ -292,9 +292,9 @@ const PoolStatistics = function (logger, client, poolConfig, portalConfig) {
             };
             console.log('mark as offline with data: ');
             console.log(workerObject);
-            // commands.push = [
-            //   ['hset', `${ _this.pool }:workers:${ blockType }:shared`, worker.worker, JSON.stringify(workerObject)],
-            // ]
+            commands.push = [
+              ['hset', `${ _this.pool }:workers:${ blockType }:shared`, worker.worker, JSON.stringify(workerObject)],
+            ]
           }
         });
       });
