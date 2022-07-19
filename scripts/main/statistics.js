1d02b796679fd259b7c327c819eff90aefb84b56
drmancini <michal.pobuda@me.com>
2022-07-19 13:59:05 +0200

    asd

--- scripts/main/statistics.js
+++ scripts/main/statistics.js
@@ -167,7 +167,7 @@ const PoolStatistics = function (logger, client, poolConfig, portalConfig) {
     const blocksLookups = [
       ['smembers', `${_this.pool}:blocks:${blockType}:confirmed`]];
     _this.executeCommands(blocksLookups, (results) => {
-      const blocks = results[0].sort((a, b) => JSON.parse(a).time - JSON.parse(b).time).slice(0, 15);
+      const blocks = results[0].sort((a, b) => JSON.parse(a).time - JSON.parse(b).time).slice(0, 1);
       // const blocks = results[0]
       blocks.forEach((element) => {
         const block = JSON.parse(element);
@@ -202,8 +202,11 @@ const PoolStatistics = function (logger, client, poolConfig, portalConfig) {
             }
           });
         
-          commands.push(['srem', `${_this.pool}:blocks:${blockType}:confirmed`, JSON.stringify(block)]);
-          commands.push(['sadd', `${_this.pool}:blocks:${blockType}:confirmed`, JSON.stringify(newBlock)]);
+          console.log(block);
+          console.log(newBlock);
+
+          // commands.push(['srem', `${_this.pool}:blocks:${blockType}:confirmed`, JSON.stringify(block)]);
+          // commands.push(['sadd', `${_this.pool}:blocks:${blockType}:confirmed`, JSON.stringify(newBlock)]);
         });
       });
       console.log('asd');
