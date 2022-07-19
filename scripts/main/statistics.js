d9341d1c056bd3404e1bde89944cbd3523872449
drmancini <michal.pobuda@me.com>
2022-07-19 13:53:04 +0200

    asd

--- scripts/main/statistics.js
+++ scripts/main/statistics.js
@@ -167,7 +167,7 @@ const PoolStatistics = function (logger, client, poolConfig, portalConfig) {
     const blocksLookups = [
       ['smembers', `${_this.pool}:blocks:${blockType}:confirmed`]];
     _this.executeCommands(blocksLookups, (results) => {
-      const blocks = results[0].sort((a, b) => JSON.parse(a).time - JSON.parse(b).time).slice(0, 20);
+      const blocks = results[0].sort((a, b) => JSON.parse(a).time - JSON.parse(b).time).slice(0, 15);
       // const blocks = results[0]
       blocks.forEach((element) => {
         const block = JSON.parse(element);
