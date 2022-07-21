aa5bb3faa329275c570a83b91b2ce8c4410a0ba6
drmancini <michal.pobuda@me.com>
2022-07-21 22:51:45 +0200

    asd

--- scripts/main/shares.js
+++ scripts/main/shares.js
@@ -168,6 +168,8 @@ const PoolShares = function (logger, client, poolConfig, portalConfig) {
 
     // Handle Shared Effort, Share Updates
     } else if (shareType === 'valid') {
+      console.log('work + ' + work);
+      console.log('times + ' + times);
       commands.push(['zadd', `${ _this.pool }:rounds:${ blockType }:current:${ minerType }:hashrate`, dateNow / 1000 | 0, JSON.stringify(hashrateShare)]);
       commands.push(['hincrby', `${ _this.pool }:rounds:${ blockType }:current:${ minerType }:counts`, 'valid', 1]);
       commands.push(['hset', `${ _this.pool }:rounds:${ blockType }:current:${ minerType }:shares`, worker, JSON.stringify(outputShare)]);
