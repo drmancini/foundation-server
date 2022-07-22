4d3d23bbf10fe4ac3fa6f39e8a642a51bd784479
drmancini <michal.pobuda@me.com>
2022-07-22 13:55:20 +0200

    asd

--- scripts/main/api2.js
+++ scripts/main/api2.js
@@ -157,9 +157,7 @@ const PoolApi = function (client, sequelize, poolConfigs, portalConfig) {
       //     })
       //   }, callback);
       
-      callback(200, {
-        result: 'ok',
-      });
+      
     }, callback);
   };
 
