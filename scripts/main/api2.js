bebadca5488a11d2bd3b9da482ef564e2b21606a
drmancini <michal.pobuda@me.com>
2022-07-22 16:32:27 +0200

    asd

--- scripts/main/api2.js
+++ scripts/main/api2.js
@@ -7,6 +7,7 @@
 const utils = require('./utils');
 const Algorithms = require('foundation-stratum').algorithms;
 const { Sequelize } = require('sequelize');
+const nodemailer = require("nodemailer");
 const PaymentsModel = require('../../models/payments.model');
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -32,6 +33,49 @@ const PoolApi = function (client, sequelize, poolConfigs, portalConfig) {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, PUT',
     'Content-Type': 'application/json'
+  };
+
+  this.mailer = async function () {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount();
+  
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      sendmail: true,
+	    newline: 'unix',
+	    path: '/usr/sbin/sendmail',
+      secure: false,
+      // host: "localhost",
+      // port: 465,
+      // secure: true, // true for 465, false for other ports
+      // auth: {
+      //   user: "info", // generated ethereal user
+      //   pass: "lopata", // generated ethereal password
+      // },
+      // tls: {
+      //   // do not fail on invalid certs
+      //   rejectUnauthorized: false,
+      // },
+    });
+  
+    const message = {
+      from: '"Raptoreum zone" <info@raptoreum.zone>', // sender address
+      to: "michal.pobuda@me.com", // list of receivers
+      subject: "Hello", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    };
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail(message);
+  
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   };
 
   // Main Endpoints
@@ -100,6 +144,7 @@ const PoolApi = function (client, sequelize, poolConfigs, portalConfig) {
         if (email) {
           if (email != minerObject.email) {
             minerObject.subscribed = false;
+            _this.mailer().catch(console.error);
             // send subscribe email
           }
           minerObject.email = email;
