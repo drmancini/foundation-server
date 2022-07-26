8fdcc0891c99bd420aa50765d7d530bf06c8939e
drmancini <michal.pobuda@me.com>
2022-07-26 13:47:30 +0200

    asd

--- scripts/main/statistics.js
+++ scripts/main/statistics.js
@@ -272,11 +272,8 @@ const PoolStatistics = function (logger, client, poolConfig, portalConfig) {
       const workersOffline = {};
 
       // Find all subscribed miners with notifications set 
-      console.log(results[0]);
       for (const [key, value] of Object.entries(results[0])) {
         const miner = JSON.parse(value);
-        console.log(key);
-        console.log(miner);
         if (miner.subscribed == true && miner.activityAlerts == true) {
           console.log('herts');
           minerNotifications.push({
