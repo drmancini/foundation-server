68227d2517d7d42347396749303bc972e0b3aac9
drmancini <michal.pobuda@me.com>
2022-07-28 16:42:53 +0200

    asd

--- scripts/main/payments.js
+++ scripts/main/payments.js
@@ -878,9 +878,9 @@ const PoolPayments = function (logger, client, sequelize) {
     // Update Worker Shares
     const deleteCurrent = function(round, pool, blockType) {
       return [
-        // ['del', `${ pool }:rounds:${ blockType }:round-${ round.height }:work`],
+        ['del', `${ pool }:rounds:${ blockType }:round-${ round.height }:work`],
         ['del', `${ pool }:rounds:${ blockType }:round-${ round.height }:counts`],
-        // ['del', `${ pool }:rounds:${ blockType }:round-${ round.height }:shares`]
+        ['del', `${ pool }:rounds:${ blockType }:round-${ round.height }:shares`]
       ];
     };
 
