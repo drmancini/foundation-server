5eaf0411366daa22285d3865b556c3e7c64f7203
drmancini <michal.pobuda@me.com>
2023-03-01 08:56:09 +0000

    redis upgrade

--- scripts/main/database.js
+++ scripts/main/database.js
@@ -22,9 +22,13 @@ const PoolDatabase = function(portalConfig) {
   this.buildRedisClient = function() {
 
     // Build Connection Options
-    const connectionOptions = {};
-    connectionOptions.port = _this.portalConfig.redis.port;
-    connectionOptions.host = _this.portalConfig.redis.host;
+    const connectionOptions = {
+      legacyMode: true,
+      socket: {
+        host: _this.portalConfig.redis.host,
+        port: _this.portalConfig.redis.port,
+      },
+    };
 
     // Check if Authentication is Set
     if (_this.portalConfig.redis.password !== '') {
@@ -33,20 +37,23 @@ const PoolDatabase = function(portalConfig) {
 
     // Check if TLS Configuration is Set
     if (_this.portalConfig.redis.tls) {
-      connectionOptions.tls = {};
-      connectionOptions.tls.key = fs.readFileSync(path.join('./certificates', _this.portalConfig.tls.key));
-      connectionOptions.tls.cert = fs.readFileSync(path.join('./certificates', _this.portalConfig.tls.cert));
-      connectionOptions.tls.ca = fs.readFileSync(path.join('./certificates', _this.portalConfig.tls.ca));
+      connectionOptions.socket.tls = true;
+      connectionOptions.socket.key = fs.readFileSync(path.join('./certificates', _this.portalConfig.tls.key));
+      connectionOptions.socket.cert = fs.readFileSync(path.join('./certificates', _this.portalConfig.tls.cert));
+      connectionOptions.socket.ca = fs.readFileSync(path.join('./certificates', _this.portalConfig.tls.ca));
     }
 
-    return redis.createClient(connectionOptions);
+    const client = redis.createClient(connectionOptions);
+    client.connect();
+
+    return client;
   };
 
   // Check Redis Client Version
   this.checkRedisClient = function(client) {
     client.info((error, response) => {
       if (error) {
-        console.log('Redis version check failed');
+        console.log(error);
         return;
       }
       let version;
